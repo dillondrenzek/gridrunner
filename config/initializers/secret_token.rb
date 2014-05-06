8ff20d50db914313e07d8068182c57e2ee4485af
Dillon Drenzek <dr.enzek@gmail.com>
2014-05-05 21:18:14 -0600

    initialized rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gridrunner::Application.config.secret_key_base = '22b8d98885a27c002a58ca417b6355d8fc7ca004df8c1a731662d9a9a109b0bcfe0f6580e2a87ee71b94832be4bd5c73eb94a1dc6be7ffcbb128e334fa6a6b6a'
