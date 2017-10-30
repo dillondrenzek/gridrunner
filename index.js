3fbe8d26c279ded1c64972320a3e6eed3c54113f
Dillon Drenzek <dr.enzek@gmail.com>
2017-10-30 16:53:41 -0700

    v2.0.0-alpha.1 - updated Gridrunner package structure

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+// Default Gridrunner constructor object
+const default_config = {
+  start: null,
+  finish: null,
+  walls: []
+};
 
 
-
-
-
-
-
-// The exported function
-// @param opts an options object used to configure the grid in a specific way
+// The Gridrunner game factory method
+// @param config a config object used to configure the grid in a specific way
 // @returns Gridrunner object with all the data and functions needed to play the game
-function gridrunner(opts) {
+function gridrunner(config) {
+  config = config || default_config;
+  const grid = buildGrid(config['start'], config['finish'], config['walls']);
+
   return {
-    CellTypeEnum,
-    grid: [],
-    player: {}
+    grid,
+    player: config['start']
   };
 };
 
-module.exports = gridrunner;
+
+
+// Builds a grid
+// @param startPos
+// @param finishPos
+// @param walls - array of grid positions for each Wall
+function buildGrid(startPos, finishPos, walls) {
+
+}
 
 
 
@@ -25,6 +36,14 @@ const Finish = 'F',
       Touched = 'T',
       Untouched = 'U',
       Wall = 'W';
+
+const CELL_TYPE_NAMES = [
+  'Finish',
+  'Start',
+  'Touched',
+  'Untouched',
+  'Wall'
+];
 
 const CellTypeEnum = {
   full: {
@@ -40,5 +59,22 @@ const CellTypeEnum = {
     T: Touched,
     U: Untouched,
     W: Wall
+  },
+  types: CELL_TYPE_NAMES,
+
+  // !untested
+  // @returns boolean - true if the value can be recognized as a valid CellType
+  isCellType: (val) => {
+    if (typeof val === 'string') {
+      if (val.length === 1) {
+        return ['F','S','T','U','W'].indexOf(val) !== -1;
+      } else {
+        return CELL_TYPE_NAMES.map(t => t.toLowerCase()).indexOf(val.toLowerCase()) !== -1;
+      }
+    }
   }
 }
+
+
+
+module.exports = {gridrunner, CellTypeEnum};
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,17 +1,46 @@
-let helpers = require('./helpers');
-let gridrunner = require('../index');
+const gridrunner = require('../index');
+const { CellTypeEnum } = gridrunner;
+
+// shorthand for readability
+const CellType = CellTypeEnum.full;
+
+const isPosition = (p) => {
+  return Array.isArray(p)
+    && p.length === 2
+    && typeof p[0] === 'number'
+    && typeof p[1] === 'number';
+}
+
+const equalPositions = (p1, p2) => {
+  if (isPosition(p1) && isPosition(p2)) {
+    return (p1[0] === p2[0]) && (p1[1] === p2[1]);
+  } else {
+    throw new Error('Need two arrays with length of 2.');
+  }
+};
+
+const isGridCellAtPostionOfType = (grid, position, cellType) => {
+  if (!isPosition(position)) throw new Error('Need a valid position.');
+  if (!gridrunner.CellTypeEnum.isCellType(cellType)) throw new Error('Need a valid CellTypeEnum type');
+  // return grid[position[0]][position[1]] ===
+};
+
 
 
 describe('Gridrunner', function() {
 
-  describe('exported function', function() {
+  describe('exported module', function() {
 
-    it('should be a function', function() {
-      expect(typeof gridrunner).toBe('function');
+    it('should be an object', function() {
+      expect(typeof gridrunner).toBe('object');
     });
 
-    it('returns an object', function() {
-      expect(typeof gridrunner()).toBe('object');
+    it('should contain the Gridrunner factory method', function() {
+      expect(typeof gridrunner.gridrunner).toBe('function');
+    });
+
+    it('should contain an enum with information about the different cell types', function() {
+      expect(typeof gridrunner.CellTypeEnum).toBe('object');
     });
 
   });
@@ -20,12 +49,12 @@ describe('Gridrunner', function() {
 
 
 
-  describe('instance created with no options', function() {
+  describe('instances', function() {
 
     let instance;
 
     beforeEach(function() {
-      instance = gridrunner();
+      instance = gridrunner.gridrunner();
     });
 
     describe('should have the property:', function() {
@@ -35,9 +64,6 @@ describe('Gridrunner', function() {
       it('player', function() {
         expect(instance.hasOwnProperty('player')).toBe(true);
       });
-      it('CellTypeEnum', function() {
-        expect(instance.hasOwnProperty('CellTypeEnum')).toBe(true);
-      });
     });
   });
 
@@ -45,12 +71,70 @@ describe('Gridrunner', function() {
 
 
 
-
   describe('instance created with valid options object', function() {
 
+    const valid_opts = {
+        start: [0,0],
+        finish: [3,0],
+        walls: [ [2,2], [3,2] ],
+        size: {
+          width: 4,
+          height: 3
+        }
+      };
+
+    it('should not throw', function() {
+      expect(() => gridrunner.gridrunner(valid_opts)).not.toThrow();
+    });
+
+    it('should set player\'s position to equal the Start position', function() {
+      const g = gridrunner.gridrunner(valid_opts);
+      const _equalPositions = equalPositions(g.player, valid_opts.start);
+      expect(_equalPositions).toBe(true);
+    });
+
+    describe('should correctly place tiles on the grid:', function() {
+      let game, grid, opts;
+
+      beforeEach(function() {
+        opts = valid_opts;
+        game = gridrunner.gridrunner(opts);
+        grid = game.grid;
+      });
+
+      it('Start', function() {
+        let position = opts.start;
+        expect(isGridCellAtPostionOfType(grid, position, CellType.Start)).toBe(true);
+      });
+      it('Finish', function() {
+        let position = opts.finish;
+        expect(isGridCellAtPostionOfType(grid, position, CellType.Finish)).toBe(true);
+      });
+      it('Wall', function() {
+        for (let i = 0; i < opts.walls; i++) {
+          let position = opts.walls[i];
+          expect(isGridCellAtPostionOfType(grid, position, CellType.Wall)).toBe(true);
+        }
+      });
+      // xit('Touched', function() {
+      //   expect(isGridCellAtPostionOfType(grid, opts[], game
+      // });
+      // xit('Untouched', function() {
+      //   expect(isGridCellAtPostionOfType(grid, opts[], game
+      // });
+
+
+    });
+
+  });
+
+
+
+
+
+  xdescribe('instance created with invalid input', function() {
+
     let instance, opts;
-
-
 
     beforeEach(function() {
       opts = {
@@ -65,24 +149,11 @@ describe('Gridrunner', function() {
       instance = gridrunner(opts);
     });
 
-    describe('should have the property:', function() {
-      it('grid', function() {
-        expect(instance.hasOwnProperty('grid')).toBe(true);
-      });
-      it('player', function() {
-        expect(instance.hasOwnProperty('player')).toBe(true);
-      });
-      it('CellTypeEnum', function() {
-        expect(instance.hasOwnProperty('CellTypeEnum')).toBe(true);
-      });
+    describe('should throw when created with', function() {
+
     });
 
-
-
-
   });
-
-
 
 
 
